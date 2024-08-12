bb1a3911cbdf2e20ec43145cd443c9d7b02b0168
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-11 19:37:06 -0500

    Wrapped RootLayout children with custom Provider component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,9 +26,9 @@ export default function RootLayout({
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { 
+        variables: {
           colorPrimary: "#3371FF",
-          fontSize: '16px'
+          fontSize: "16px",
         },
       }}
     >
@@ -38,7 +39,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
