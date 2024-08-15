a04fe4b5779a801c6e0f7c047defe3d56948f17a
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-15 00:26:57 -0500

    A feature has been implemented to retrieve all documents associated with a specific user, using their email for filtering.

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -54,7 +54,7 @@ export const getDocument = async ({
 
     return parseStringify(room);
   } catch (error) {
-    console.log(`Error happened while getting room: ${error}`);
+    console.log(`Error happened while getting a room: ${error}`);
   }
 };
 
@@ -71,5 +71,16 @@ export const updateDocument = async (roomId: string, title: string) => {
     return parseStringify(updatedRoom);
   } catch (error) {
     console.log(`Error happened while updating  a room: ${error}`);
+  }
+};
+
+export const getDocuments = async (
+  email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while getting rooms: ${error}`);
   }
 };
