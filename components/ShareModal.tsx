5c658c87562ca9b292bedfd06e36c85e418c91c3
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-24 00:05:32 -0500

    Added Collaborator list to ShareModal, integrating existing collaborators into the document sharing interface

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -16,6 +16,7 @@ import Image from "next/image";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import UserTypeSelector from "./UserTypeSelector";
+import Collaborator from "./Collaborator";
 
 const ShareModal = ({
   roomId,
@@ -80,6 +81,20 @@ const ShareModal = ({
           >
             {loading ? "Sending..." : "Invite"}
           </Button>
+        </div>
+        <div className="my-2 space-y-2">
+          <ul className="flex flex-col">
+            {collaborators.map((collaborator) => (
+              <Collaborator
+                key={collaborator.id}
+                roomId={roomId}
+                creatorId={creatorId}
+                email={collaborator.email}
+                collaborator={collaborator}
+                user={user.info}
+              />
+            ))}
+          </ul>
         </div>
       </DialogContent>
     </Dialog>
