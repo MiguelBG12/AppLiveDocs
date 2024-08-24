7a4adf84b16cfdc1386f7971a17adb002d5f0e12
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-23 20:55:04 -0500

    Enhanced ShareModal with UI components for managing document access and email input functionality

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { useSelf } from "@liveblocks/react/suspense";
 import React, { useState } from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import UserTypeSelector from "./UserTypeSelector";
 
 const ShareModal = ({
   roomId,
@@ -17,9 +31,51 @@ const ShareModal = ({
   const [email, setEmail] = useState("");
   const [userType, setUserType] = useState<UserType>("viewer");
 
-  const shareDocumentHandler = async () => {}
-  
-  return <div>ShareModal</div>;
+  const shareDocumentHandler = async () => {};
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button
+          className="gradient-blue flex h-9 gap-1 px4"
+          disabled={currentUserType !== "editor"}
+        >
+          <Image
+            src="/assets/icons/share.svg"
+            alt="share"
+            width={20}
+            height={20}
+            className="min-w-4 md:size-5"
+          />
+          <p className="mr-1 hidden sm:block">Share</p>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog">
+        <DialogHeader>
+          <DialogTitle>Manage who can view this project</DialogTitle>
+          <DialogDescription>
+            Select which users can view and edit this document
+          </DialogDescription>
+        </DialogHeader>
+
+        <Label htmlFor="email" className="mt-6 text-blue-100">
+            Email address
+        </Label>
+        <div className="flex items-center gap-3">
+            <div className="flex flex-1 rounded-md bg-dark-400">
+                <Input 
+                    id="email"
+                    placeholder="Enter email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="share-input"
+                />
+            </div>
+
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default ShareModal;
